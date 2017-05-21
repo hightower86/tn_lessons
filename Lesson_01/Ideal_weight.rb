b9d5d1ea761ff44df12433de800ca7fa3e8ab15b
hightower <smlog3@bk.ru>
2017-05-21 13:46:57 +0300

    new lesson 01

---
+++ Lesson_01/Ideal_weight.rb
@@ -0,0 +1,14 @@
+
+
+puts "Какой Вас зовут ?"
+u_name = gets.chomp
+
+puts "Какой Ваш рост (в сантиметрах) ?"
+height = gets.chomp.to_i
+ideal_weight = height - 110
+
+if ideal_weight < 0 
+	puts "#{u_name}, Ваш вес уже оптимальный!"
+else
+	puts "#{u_name}, Ваш идеальный вес #{ideal_weight} кг."
+	end
