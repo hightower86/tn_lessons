ebb129af8b0e32082aaeeab85b77200c3707be20
hightower <smlog3@bk.ru>
2017-06-21 20:21:41 +0300

    part 5redact. 4th

--- Lesson_05/modules.rb
+++ Lesson_05/modules.rb
@@ -1,15 +1,7 @@
 module Manufacturer
 
-  def set_manufacturer(title)
-    self.manufacturer = title
-  end
-
-  def manufacturer
-    self.manufacturer
-  end
-
-  protected
   attr_accessor :manufacturer
+
 end
 
 module InstanceCounter
@@ -26,9 +18,7 @@ module InstanceCounter
     end
 
     def counter_up
-      if @instances == nil
-        @instances = 0
-      end
+      @instances ||= 0
       @instances += 1
     end
   end
--- Lesson_05/train.rb
+++ Lesson_05/train.rb
@@ -1,4 +1,3 @@
-
 class Train
 
   include Manufacturer
