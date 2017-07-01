491db34fb66a8f80bf761a1fcb385979bdb0c3f2
hightower <smlog3@bk.ru>
2017-07-01 13:56:53 +0300

    6 task 1st edition

---
+++ Lesson_06/modules.rb
@@ -0,0 +1,32 @@
+module Manufacturer
+
+  attr_accessor :manufacturer
+
+end
+
+module InstanceCounter
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.include     InstanceMethods
+  end
+
+  module ClassMethods
+    
+    def instances
+      @instances
+    end
+
+    def counter_up
+      @instances ||= 0
+      @instances += 1
+    end
+  end
+
+  module InstanceMethods
+    protected
+    def register_instance
+      self.class.send :counter_up
+    end
+  end
+end
---
+++ Lesson_06/route.rb
@@ -0,0 +1,26 @@
+class Route
+  
+  attr_reader :route
+
+  def initialize(start_station, end_station)
+    @route = [start_station, end_station]
+    register_instance
+  end
+
+  def add_station(station)
+    @route.insert(-2, station)
+  end
+
+  def del_station(station)
+    if @route.include? station
+      @route.delete(station)
+    else
+      raise "Станции #{station} нет в маршруте!"
+    end
+  end
+
+  def get_station_list
+     @route.each { |e| puts e }
+  end
+
+end
